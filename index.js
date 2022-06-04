cb458fa31c8063516a26394241ba93d13b1440d6
Yoverina Nurdin <yoverina_nurdin@yahoo.co.id>
2022-06-05 03:26:04 +0700

    add sorter function

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arr, sort) => {
+  let sortedArr = sort(arr);
+  for (let i = 0; i < sortedArr.length; i++){
+    sortedArr[i] = `${i+1}. ${sortedArr[i]}`;
+  }
+  return sortedArr;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arr) =>{
+  tempArr = arr.slice();
+  sortedArr = tempArr.sort();
+  return sortedArr;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arr) =>{
+  tempArr = arr.slice();
+  sortedArr2 = tempArr.sort();
+  sortedArr2.reverse();
+  return sortedArr2;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
